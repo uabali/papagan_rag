af3309001cbbe74bc354398c0e935271845b5835
Taha Lenger <tahalenger@gmail.com>
2025-12-19 00:15:39 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.llms import Ollama
-
+##
 load_dotenv()
 PDF_FOLDER = "data"
 DB_DIR = "./chroma_db"
